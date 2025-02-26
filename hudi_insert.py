e51edd89aea564cde700a566955f85d7f28aa492
Vishal Kaushik <vishalkaushik2002@gmail.com>
2025-02-26 21:12:02 +0530

    Added the use-cases for insert, upsert and update

--- hudi_insert.py
+++ hudi_insert.py
@@ -26,3 +26,47 @@ columns = ["id", "name", "age", "ts"]
 df = spark.createDataFrame(data, columns)
 print("*** df ***")
 print(df)
+
+
+# Define Hudi Table Path and Options
+hudi_table_path = "file:///tmp/test_table"
+hudi_table_name = "hudi_persons"
+
+hudi_options = {
+    "hoodie.table.name": hudi_table_name,
+    "hoodie.datasource.write.operation": "insert",
+    "hoodie.datasource.write.recordkey.field": "id",
+    "hoodie.datasource.write.precombine.field": "ts",
+    "hoodie.datasource.write.partitionpath.field": "",
+    "hoodie.datasource.hive_sync.enable": "false",
+    "hoodie.datasource.write.table.type": "COPY_ON_WRITE",
+}
+
+# Write Data as Hudi Table
+df.write.format("hudi").options(**hudi_options).mode("overwrite").save(hudi_table_path)
+print("*** Hudi table created successfully! ***")
+
+update_data = [
+    ("2", "Bob", 32, "2024-02-26T12:00:00"),
+    ("4", "David", 29, "2024-02-26T12:01:00")
+]
+update_df = spark.createDataFrame(update_data, columns)
+
+update_options = {
+    "hoodie.table.name": hudi_table_name,
+    "hoodie.datasource.write.operation": "upsert",
+    "hoodie.datasource.write.recordkey.field": "id",
+    "hoodie.datasource.write.precombine.field": "ts",
+    "hoodie.datasource.write.partitionpath.field": "",
+    "hoodie.datasource.hive_sync.enable": "false",
+    "hoodie.datasource.write.table.type": "COPY_ON_WRITE",
+}
+
+# Upsert Data
+update_df.write.format("hudi").options(**update_options).mode("append").save(hudi_table_path)
+print("*** Hudi table updated successfully! ***")
+
+
+updated_hudi_df = spark.read.format("hudi").load(hudi_table_path)
+print("*** updated_hudi_df ***")
+print(updated_hudi_df)
